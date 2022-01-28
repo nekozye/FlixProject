189f6e743b8e81d084e04e698776912f0a35d13d
nekozye <nekozeye@gmail.com>
2022-01-28 04:14:55 -0500

    started on Movie class. before moving update

--- app/src/main/java/com/neko/flixproject/Movie.java
+++ app/src/main/java/com/neko/flixproject/Movie.java
@@ -1,4 +1,14 @@
 package com.neko.flixproject;
 
+import org.json.JSONObject;
+
 public class Movie {
+        String posterPath;
+        String title;
+        String overView;
+
+        public Movie(JSONObject jsonObject){
+            posterPath = jsonObject.getString("poster_path");
+        }
+
 }
